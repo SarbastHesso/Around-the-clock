ace4e48dddd628fc333babcd68962c1df2b18111
Sarbast Hesso <sarbast.hesso@gmail.com>
2023-12-20 11:01:56 +0100

    text och digital form

--- script.js
+++ script.js
@@ -1,33 +1,100 @@
-const hour = document.querySelector('#hour');
-const minute = document.querySelector('#minute');
-const time24 = document.querySelector('#time-24');
-const timeText = document.querySelector('#time-text');
-const btn = document.querySelector('#btn');
+const hour = document.querySelector("#hour");
+const minute = document.querySelector("#minute");
+const timeOutput = document.querySelector("#time-output");
+const time24 = document.querySelector("#time-24");
+const timeText = document.querySelector("#time-text");
+const textBtn = document.querySelector("#text-btn");
+const digitalBtn = document.querySelector("#digital-btn");
 
-setInterval (() => {
+setInterval(() => {
+  let time = new Date();
+  let hr = time.getHours();
+  let min = time.getMinutes();
 
-    let time = new Date();
-    let hr = time.getHours();
-    let min = time.getMinutes();
+  hr_rotation = 30 * hr + min / 2;
+  min_rotation = 6 * min;
 
-    hr_rotation = 30 * hr + min / 2;
-    min_rotation = 6 * min;
+  hour.style.transform = `rotate(${hr_rotation}deg)`;
+  minute.style.transform = `rotate(${min_rotation}deg)`;
 
-    hour.style.transform = `rotate(${hr_rotation}deg)`;
-    minute.style.transform = `rotate(${min_rotation}deg)`;
+  time24.innerHTML = hr + ":" + min;
 
-    time24.innerHTML = hr + ':' + min;
+  let minutesArray = [
+    "",
+    "fem över",
+    "tio över",
+    "kvart över",
+    "tjugo över",
+    "fem i halv",
+    "halv",
+    "fem över halv",
+    "tjugo i",
+    "kvart i",
+    "tio i",
+    "fem i",
+    "",
+  ];
 
-}, 1000)
+  let minuteText = minutesArray[Math.round(min / 5)];
 
+  let hoursArray = [
+    "tolv",
+    "ett",
+    "två",
+    "tre",
+    "fyra",
+    "fem",
+    "sex",
+    "sju",
+    "åtta",
+    "nio",
+    "tio",
+    "elva",
+    "tolv",
+    "ett",
+    "två",
+    "tre",
+    "fyra",
+    "fem",
+    "sex",
+    "sju",
+    "åtta",
+    "nio",
+    "tio",
+    "elva",
+    "tolv",
+  ];
 
-btn.addEventListener ('click', (e) => {
-    e.preventDefault()
-    if (btn.textContent === 'Visa i digital form'){
-        btn.textContent = "Dölj i digital form";
-        timeText.classList.add('show-text')
-    } else {
-        btn.textContent = "Visa i digital form";
-        timeText.classList.remove('show-text')
-    }
-})
+  let hourText = hoursArray[min > 29 ? hr + 1 : hr];
+
+  timeText.innerHTML = `${minuteText} ${hourText}`;
+  
+}, 1000);
+
+digitalBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (digitalBtn.textContent === "Visa i digital form") {
+    digitalBtn.textContent = "Dölj i digital form";
+    timeText.style.display = "none";
+    time24.style.display = "inline-block";
+    timeOutput.classList.add("show-output");
+    textBtn.textContent = "Visa tiden i text";
+  } else {
+    digitalBtn.textContent = "Visa i digital form";
+    timeOutput.classList.remove("show-output");
+  }
+});
+
+textBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (textBtn.textContent === "Visa tiden i text") {
+    textBtn.textContent = "Dölj tiden i text";
+    timeText.style.display = "inline-block";
+    time24.style.display = "none";
+    timeOutput.classList.add("show-output");
+    digitalBtn.textContent = "Visa i digital form";
+  } else {
+    textBtn.textContent = "Visa tiden i text";
+    timeOutput.classList.remove("show-output");
+  }
+});
